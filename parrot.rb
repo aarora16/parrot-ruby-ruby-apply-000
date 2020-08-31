43de00e1cb625a248e9c8b4fbf07937b5570c095
Anshul Arora <a.99.anshul@gmail.com>
2020-08-31 23:20:41 +0000

    Automatically backed up by Learn

--- parrot.rb
+++ parrot.rb
@@ -1,6 +1,6 @@
 # Create method `parrot` that outputs a given phrase and
 # returns the phrase
-def parrot(bird)
+def parrot(bird = "Squawk!")
   puts "Hi I'm a #{bird}"
   return "Hi I'm a #{bird}"
 end
